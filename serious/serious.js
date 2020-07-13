4f16478038eb4fd5b600d127e85eac3e4845fc41
Hamza-BenJabeur <Hamza.mhbj@gmail.com>
2020-07-13 12:06:38 +0200

    done

--- serious/serious.js
+++ serious/serious.js
@@ -25,7 +25,7 @@
 		
 
 		choice = [];
-		$ ('#revenue').html(anotherChoice());
+		$ ('#revenue').html(anotherChoice()+'dt');
 	}
 		
 	
@@ -39,8 +39,9 @@
     	var choice=[];
 	function makecoffe(CoffeType){
 	choice.push(CoffeType);
-	// console.log(choice);
-	  	$("#img").attr('src','https://imgaz.staticbg.com/thumb/view/oaupload/ser1/banggood/images/3B/F4/bf29ad2d-4734-4f57-b1c9-dad5c497a1ae.png');
+	if((CoffeType==="brew")||(CoffeType==="espresso")){
+	  	$("#img").attr('src','https://imgaz.staticbg.com/thumb/view/oaupload/ser1/banggood/images/3B/F4/bf29ad2d-4734-4f57-b1c9-dad5c497a1ae.png ');
+	
 	setTimeout(function(){
 $("#img").attr("src","https://images.homedepot-static.com/productImages/23e773bd-fe78-4ee8-90b7-820c6c2c5767/svn/allures-illusions-mugs-giant-coffee-64_600.jpg");
 	},1000);
@@ -48,6 +49,33 @@ $("#img").attr("src","https://images.homedepot-static.com/productImages/23e773bd
 $("#img").attr("src","https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
 },1500);
 	 $('#revenue').html(anotherChoice()+'dt');
-}
+	}
+	else if (CoffeType==="Latte"){
+		  	$("#img").attr('src','https://imgaz.staticbg.com/thumb/view/oaupload/ser1/banggood/images/3B/F4/bf29ad2d-4734-4f57-b1c9-dad5c497a1ae.png ');
+	
+	setTimeout(function(){
+$("#img").attr("src","https://media-cdn.tripadvisor.com/media/photo-s/12/8a/39/6c/un-bon-latte.jpg");
+	},1000);
+	setTimeout(function(){
+$("#img").attr("src","https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+},1500);
+	 $('#revenue').html(anotherChoice()+'dt');
+	}
+	else if(CoffeType==="hot"){
+	$("#img").attr('src','https://imgaz.staticbg.com/thumb/view/oaupload/ser1/banggood/images/3B/F4/bf29ad2d-4734-4f57-b1c9-dad5c497a1ae.png ');
+	
+	setTimeout(function(){
+$("#img").attr("src","https://images-na.ssl-images-amazon.com/images/I/517cCg8KCAL._AC_.jpg");
+	},1000);
+	setTimeout(function(){
+$("#img").attr("src","https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80");
+},1500);
+	 $('#revenue').html(anotherChoice()+'dt');
+
+		}
+
+
+	}
+
 
 
